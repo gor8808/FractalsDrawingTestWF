1611d0c1f354e2f4b38f80b9ffa44fb304fc683c
Gor Grigoryan <gor8808@users.noreply.github.com>
2020-05-13 14:46:23 +0400

    Initial commit

---
+++ MultiPageExample/Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace MultiPageExample
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.picCanvas = new System.Windows.Forms.PictureBox();
+            this.LengthValue = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.LengthScaleValue = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label4 = new System.Windows.Forms.Label();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.button3 = new System.Windows.Forms.Button();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.picCanvas)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.LengthValue)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.LengthScaleValue)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picCanvas
+            // 
+            this.picCanvas.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.picCanvas.Location = new System.Drawing.Point(0, 99);
+            this.picCanvas.Name = "picCanvas";
+            this.picCanvas.Size = new System.Drawing.Size(1096, 581);
+            this.picCanvas.TabIndex = 0;
+            this.picCanvas.TabStop = false;
+            this.picCanvas.Paint += new System.Windows.Forms.PaintEventHandler(this.picCanvas_Paint);
+            // 
+            // LengthValue
+            // 
+            this.LengthValue.Location = new System.Drawing.Point(104, 12);
+            this.LengthValue.Name = "LengthValue";
+            this.LengthValue.Size = new System.Drawing.Size(120, 22);
+            this.LengthValue.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(11, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 17);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Length";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(11, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(87, 17);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "LengthScale";
+            // 
+            // LengthScaleValue
+            // 
+            this.LengthScaleValue.Location = new System.Drawing.Point(104, 41);
+            this.LengthScaleValue.Name = "LengthScaleValue";
+            this.LengthScaleValue.Size = new System.Drawing.Size(120, 22);
+            this.LengthScaleValue.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(246, 41);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(79, 17);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "AngleScale";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(339, 41);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 22);
+            this.numericUpDown1.TabIndex = 9;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(246, 12);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(42, 17);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Deep";
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(339, 12);
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(120, 22);
+            this.numericUpDown2.TabIndex = 7;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(465, 25);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(122, 33);
+            this.button3.TabIndex = 12;
+            this.button3.Text = "Select Color";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1095, 677);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.LengthScaleValue);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.LengthValue);
+            this.Controls.Add(this.picCanvas);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.picCanvas)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.LengthValue)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.LengthScaleValue)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        
+
+        #endregion
+        private System.Windows.Forms.PictureBox picCanvas;
+        private NumericUpDown LengthValue;
+        private Label label1;
+        private Label label2;
+        private NumericUpDown LengthScaleValue;
+        private Label label3;
+        private NumericUpDown numericUpDown1;
+        private Label label4;
+        private NumericUpDown numericUpDown2;
+        private Button button3;
+        private ColorDialog colorDialog1;
+    }
+}
+
---
+++ MultiPageExample/Form1.cs
@@ -0,0 +1,214 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace MultiPageExample
+{
+    public partial class Form1 : Form
+    {
+        private float xmin = -60;
+        private float ymin = -60;
+        private float xmax = 60;
+        private float ymax = 60;
+        PointF A = new PointF(60, 0);
+        PointF B = new PointF(0, 60);
+        PointF C = new PointF(-60, 0);
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void picCanvas_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics gr = e.Graphics;
+            gr.Clear(picCanvas.BackColor);
+            gr.SmoothingMode = SmoothingMode.AntiAlias;
+            picCanvas.BackColor = Color.Black;
+            Pen pen = new Pen(Brushes.White);
+            float height = picCanvas.Height;
+            float width = picCanvas.Height;
+            RectangleF rect = new RectangleF(
+                    xmin, ymin, xmax - xmin, ymax - ymin);
+            PointF[] pts =
+            {
+                    new PointF(0, height),
+                    new PointF(width, height),
+                    new PointF(0, 0),
+                };
+            gr.Transform = new Matrix(rect, pts);
+            pen.Color = Color.Red;
+            gr.DrawLine(pen, xmin, 0, xmax, 0);
+            gr.DrawLine(pen, 0, ymin, 0, ymax);
+            for (int x = (int)xmin; x <= xmax; x++)
+            {
+                gr.DrawLine(pen, x, -0.1f, x, 0.1f);
+            }
+            for (int y = (int)ymin; y <= ymax; y++)
+            {
+                gr.DrawLine(pen, -0.1f, y, 0.1f, y);
+            }
+
+
+
+            int cubeSide = 40;
+            //PointF centre = new PointF(0 - cubeSide / 2, 0 - cubeSide / 2);
+            pen.Color = Color.White;
+            //gr.DrawRectangle(pen, centre.X, centre.Y, cubeSide, cubeSide);
+            SierpinskiCarpet(gr, 5,new RectangleF(-60,-60,120,120));
+            //float dist1 = cubeSide / 2 + cubeSide / 3;
+            //float dist2 = 0 - dist1 - cubeSide / 3;
+            //float dist3 = cubeSide / 2 + cubeSide / 3 + ((cubeSide / 2 * (float)Math.Sqrt(2)) + (cubeSide / 3 * (float)Math.Sqrt(2)));
+            //float dist4 = 0 - cubeSide - cubeSide / 3;
+            //PointF centre1;
+            //cubeSide /= 3;
+            //centre1 = new PointF(dist1, 0 - cubeSide / 2);
+            //pen.Color = Color.Green;
+            //gr.DrawRectangle(pen, centre1.X, centre1.Y, cubeSide, cubeSide);
+
+            //centre1 = new PointF(dist2, 0 - cubeSide / 2);
+            //pen.Color = Color.Green;
+            //gr.DrawRectangle(pen, centre1.X, centre1.Y, cubeSide, cubeSide);
+
+            //centre1 = new PointF(0 - cubeSide / 2, dist1);
+            //pen.Color = Color.Green;
+            //gr.DrawRectangle(pen, centre1.X, centre1.Y, cubeSide, cubeSide);
+
+            //centre1 = new PointF(0 - cubeSide / 2, dist2);
+            //pen.Color = Color.Green;
+            //gr.DrawRectangle(pen, centre1.X, centre1.Y, cubeSide, cubeSide);
+
+            //centre1 = GetPointByLengthAndAngle(centre, dist3, (float)Math.PI / 4);
+            //pen.Color = Color.White;
+            //gr.DrawRectangle(pen, centre1.X - cubeSide/2, centre1.Y - cubeSide/2, cubeSide, cubeSide);
+
+            //centre1 = GetPointByLengthAndAngle(centre, dist3, (float)Math.PI / 4);
+            //pen.Color = Color.White;
+            //gr.DrawRectangle(pen, -1 * (centre1.X - cubeSide/2) - cubeSide / 3 * 1.5f, -1 * (centre1.Y - cubeSide/2) - cubeSide / 3 * 1.5f, cubeSide, cubeSide);
+
+        }
+
+        private void SierpinskiCarpet(Graphics gr, int level, RectangleF rect)
+        {
+            if (level == 0)
+            {
+                gr.FillRectangle(Brushes.Blue, rect);
+            }
+            else
+            {
+                // Divide the rectangle into 9 pieces.
+                float wid = rect.Width / 3;
+                float x0 = rect.Left;
+                float x1 = x0 + wid;
+                float x2 = x0 + wid * 2f;
+
+                float hgt = rect.Height / 3f;
+                float y0 = rect.Top;
+                float y1 = y0 + hgt;
+                float y2 = y0 + hgt * 2f;
+
+                SierpinskiCarpet(gr, level - 1, new RectangleF(x0, y0, wid, hgt));
+                SierpinskiCarpet(gr, level - 1, new RectangleF(x1, y0, wid, hgt));
+                SierpinskiCarpet(gr, level - 1, new RectangleF(x2, y0, wid, hgt));
+                SierpinskiCarpet(gr, level - 1, new RectangleF(x0, y1, wid, hgt));
+                SierpinskiCarpet(gr, level - 1, new RectangleF(x2, y1, wid, hgt));
+                SierpinskiCarpet(gr, level - 1, new RectangleF(x0, y2, wid, hgt));
+                SierpinskiCarpet(gr, level - 1, new RectangleF(x1, y2, wid, hgt));
+                SierpinskiCarpet(gr, level - 1, new RectangleF(x2, y2, wid, hgt));
+            }
+        }
+
+        private PointF GetPointByLengthAndAngle(PointF p1, float length, float angle)
+        {
+            return new PointF((float)(p1.X + Math.Cos(angle) * length), (float)(p1.Y + Math.Sin(angle) * length));
+        }
+        private void Start_Click(object sender, EventArgs e)
+        {
+            int height = 500;
+            int width = 500;
+            Bitmap bitmap = new Bitmap(height, width);
+            for (int i = 0; i < height; i++)
+            {
+                for (int j = 0; j < width; j++)
+                {
+                    bitmap.SetPixel(i, j, Color.Yellow);
+                }
+            }
+            picCanvas.Image = bitmap;
+            using (Graphics gr = Graphics.FromImage(bitmap))
+            {
+                gr.SmoothingMode = SmoothingMode.AntiAlias;
+
+                RectangleF rect = new RectangleF(
+                    xmin, ymin, xmax - xmin, ymax - ymin);
+                PointF[] pts =
+                {
+                    new PointF(0, height),
+                    new PointF(width, height),
+                    new PointF(0, 0),
+                };
+                gr.Transform = new Matrix(rect, pts);
+                using (Pen graph_pen = new Pen(Color.Blue, 0.1f))
+                {
+                    graph_pen.Color = Color.Red;
+                    gr.DrawLine(graph_pen, xmin, 0, xmax, 0);
+                    gr.DrawLine(graph_pen, 0, ymin, 0, ymax);
+                    for (int x = (int)xmin; x <= xmax; x++)
+                    {
+                        gr.DrawLine(graph_pen, x, -0.1f, x, 0.1f);
+                    }
+                    for (int y = (int)ymin; y <= ymax; y++)
+                    {
+                        gr.DrawLine(graph_pen, -0.1f, y, 0.1f, y);
+                    }
+                    graph_pen.Color = Color.Blue;
+
+                    gr.DrawLine(graph_pen, A.X, A.Y, B.X, B.Y);
+                    gr.DrawLine(graph_pen, B.X, B.Y, C.X, C.Y);
+                    gr.DrawLine(graph_pen, A.X, A.Y, C.X, C.Y);
+
+                    for (int i = 0; i < 100; i++)
+                    {
+                        GetPoints(A, C, out PointF p1, out PointF p2, out PointF topPoint);
+
+                        //graph_pen.Color = Color.Yellow;
+                        //graph_pen.Width = 0.5f;
+                        //gr.DrawLine(graph_pen, p1, p2);
+                        graph_pen.Color = Color.Blue;
+                        graph_pen.Width = 0.1f;
+
+                        gr.DrawLine(graph_pen, p1, topPoint);
+                        gr.DrawLine(graph_pen, p2, topPoint);
+                    }
+                }
+            }
+        }
+        private void GetPoints(PointF startP, PointF endP, out PointF p1, out PointF p2, out PointF topPoint)
+        {
+            p1 = new PointF();
+            p2 = new PointF();
+            topPoint = new PointF();
+
+            float d = GetDistance(startP, endP);
+            p1 = new PointF(((startP.X * 2) + endP.X) / 3, ((startP.Y * 2) + endP.Y) / 3);
+            p2 = new PointF((startP.X + (endP.X * 2)) / 3, (startP.Y + (endP.Y * 2)) / 3);
+
+            float angle1 = (float)Math.PI / 2.5f;
+            float length = d / 3;
+
+            topPoint = new PointF((float)(p1.X + Math.Cos(angle1) * length), (float)(p1.Y + Math.Sin(angle1) * length));
+        }
+        private float GetDistance(PointF p1, PointF p2)
+        {
+            return Convert.ToSingle(Math.Sqrt(Math.Pow(p2.X - p1.X, 2) + Math.Pow(p2.Y - p1.Y, 2)));
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (colorDialog1.ShowDialog() == DialogResult.OK)
+            {
+            }
+        }
+    }
+}
